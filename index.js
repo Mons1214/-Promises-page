35523150d370d64b83227efbf3d6aea95f135093
Mons1214 <mons.gorsor@gmail.com>
2025-06-11 18:29:17 -0600

    Added routes to the server and a button that makes it possible to navigate between routes

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-    console.log('Este es el servidor')
-    res.send('Inicio');
+    console.log('Principal')
+    res.sendFile(path.join(__dirname, 'public', 'paginaDePromesas.html'));
 });
 
-app.get('/page-two', (req, res) => {
-    console.log('Primer TE AMO')
-    res.send('Primera Ruta')
-});
-
-app.get('/page-three', (req, res) => {
-    console.log('segundo TE AMO REYSOTE')
-    res.send('Segunda Ruta')
+//ruta1
+app.get('/ruta1', (req, res) => {
+    console.log('ruta1')
+    res.sendFile(path.join(__dirname, 'public', 'ruta1', 'ruta1.html'));    
 });
 
 
-app.get('/page-four', (req,res) => {
-    console.log('Tercer TE AMO, ¡GRACIAS!')
-    res.send('Tercer Ruta')
+//ruta2
+app.get('/ruta2', (req, res) => {
+    console.log('ruta2')
+    res.sendFile(path.join(__dirname, 'public',  'ruta2', 'ruta2.html'));
 });
 
-app.listen(3000,() => { 
+
+//ruta3
+app.get('/ruta3', (req, res) => {
+    console.log('ruta3')
+    res.sendFile(path.join(__dirname, 'public',  'ruta3', 'ruta3.html'));
+});
+
+
+app.listen(PORT, () => {
     console.log('Servidor corriendo en http://localhost:3000');
 });
--- public/ruta1/ruta1.js
+++ public/ruta1/ruta1.js
@@ -2,5 +2,5 @@ let button = document.querySelector("#boton")
 
 console.log(button)
 button.addEventListener("click", () => {
-    window.location.href='/ruta1'
+    window.location.href='/ruta2'
 });
